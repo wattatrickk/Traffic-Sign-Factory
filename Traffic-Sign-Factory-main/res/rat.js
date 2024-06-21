2ca40a13ea0fd281f95077d77b1a0fbc50cc7bfc
BaoNg <wattatrick@gmail.com>
2024-06-20 23:07:43 -0500

    Add files via upload

---
+++ Traffic-Sign-Factory-main/res/rat.js
@@ -0,0 +1,27 @@
+var rats =
+[
+{
+'No' :      ['Many','Few','No'],
+'road' :          ['road','street','line','avenue','drive'],
+'is long' :   ['is short','is fun','is sad','is weird','is strange'],
+'with good company' :        ['with a good friend','with a family','with your wife','with your husband','with your dog','with your cat']
+},
+{
+'Difficult roads' :      ['Beautiful roads','Crowded roads','Village roads','Country roads'],
+'often' :          ['usually','barely','always','sometimes','never','frequently','regularly','normally'],
+'lead' :   ['guide','help','assist','show','see','take','open'],
+'to beautiful destinations' :        ['the driver','to nowhere','to heaven','to hell','to your house','yourself']
+},
+{
+'Life' :      ['Death','A Painting','Existence'],
+'is a lottery,' :          ['is a journey','is a conversation','is an experient'],
+'Be lucky' :   ['Be wild','be fun','be weird','be strange','be something different'],
+},
+{
+'Here' :      ['There','Everywhere','Those places','In my heart','In my head','On your head'],
+'today' :   ['tomorrow','next week','today','next month','next year','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'],
+'gone' :    ['done','quit','passed','left','lost','dissapear','around','dead'],
+'tomorrow' :   ['forever','afterward','again','shortly','soon','then','immediately','presently']
+}
+
+];
---
+++ Traffic-Sign-Factory-main/res/ratking.js
@@ -0,0 +1,12 @@
+var ratKing = [
+  'Why do they call it rush hour when nothing moves?',
+  'I could not repair your brakes, so I made your horn louder.',
+  'Americans will put up with anything provided it does not block traffic.',
+  'No road is long with good company.',
+  'Kilometers are shorter than miles. Save gas, take your next trip in kilometers.',
+  'Road Trips: Because they are cheaper than therapy.',
+  'Difficult roads often lead to beautiful destinations.',
+  'When you go on a road trip, the trip itself becomes part of the story.',
+  'Here today, gone tomorrow.',
+  'Never have more children than you have car windows.'
+];
